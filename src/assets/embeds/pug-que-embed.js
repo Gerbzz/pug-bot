c27a1caf9f0ca5ce340f842f3418c929bd08d7ac
Gerbzz <gerbeneil@gmail.com>
2024-03-18 18:30:03 -0400

    updated pug que embed

--- src/assets/embeds/pug-que-embed.js
+++ src/assets/embeds/pug-que-embed.js
@@ -13,7 +13,7 @@ const pugModel = require("../../models/pug-model");
 const gifUrl =
 	"https://media1.tenor.com/m/1YgYDAoufH0AAAAC/winditup-tiktok.gif";
 const thumbnailUrl =
-	"https://cdn.discordapp.com/attachments/549053891476455436/677649538214920217/black_bar_transparent.png1.png?ex=6584c2d0&is=65724dd0&hm=c832f28a000b02d7417a260a716a95e917e36ed3adae3db6bdeff92d14b6341a&";
+	"https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzJqeDJxYTQ3aXU5N3E1cHR2bnVrZTR3MXMzc3I4c3NrY2N1cHUydSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUPGcLvo0MIkriPh2o/giphy.gif";
 
 // add the doc from the database to the function parameters
 
