9cfdaa66c285c36e76b3747bbc4ead91db5fe9af
Gerbzz <gerbeneil@gmail.com>
2024-03-21 00:06:58 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 /** @format */
 
+// /** @format */
+
+// // index.js
+// require("dotenv").config();
+// const { Client, GatewayIntentBits } = require("discord.js");
+// const eventHandler = require("./src/handlers/event-handler.js");
+// const keepAlive = require("./keep-alive.js");
+// // Connect to MongoDB
+// const connectDB = require("./src/database/db.js");
+// connectDB();
+
+// const client = new Client({
+// 	intents: [
+// 		GatewayIntentBits.Guilds,
+// 		GatewayIntentBits.GuildVoiceStates,
+// 		GatewayIntentBits.GuildMembers,
+// 		GatewayIntentBits.GuildMessageReactions,
+// 	],
+// });
+
+// eventHandler(client);
+// keepAlive();
+// client.login(process.env.DISCORD_TOKEN);
+
 // index.js
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 const eventHandler = require("./src/handlers/event-handler.js");
 const keepAlive = require("./keep-alive.js");
+
 // Connect to MongoDB
 const connectDB = require("./src/database/db.js");
-connectDB();
 
+// Create a new Discord client instance
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -18,6 +43,22 @@ const client = new Client({
 	],
 });
 
-eventHandler(client);
-keepAlive();
-client.login(process.env.DISCORD_TOKEN);
+// This function will start the bot after ensuring the database is connected
+async function startBot() {
+	try {
+		// Connect to the database
+		await connectDB();
+		// Database is connected, now we can start the event handling and keepAlive server
+		eventHandler(client);
+		keepAlive();
+		// After database and keepAlive, log in to Discord
+		await client.login(process.env.DISCORD_TOKEN);
+		console.log("Bot is logged in and ready to serve requests");
+	} catch (error) {
+		console.error("Error starting the bot", error);
+		process.exit(1);
+	}
+}
+
+// Call the function to start the bot
+startBot();
